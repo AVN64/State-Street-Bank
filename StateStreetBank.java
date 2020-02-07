c5520d92c5f05d999c97ffb724fc30a5e09557eb
AVN64 <60475192+AVN64@users.noreply.github.com>
2020-02-07 13:50:21 -0500

    Add files via upload

---
+++ StateStreetBank.java
@@ -0,0 +1,88 @@
+import java.io.File;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class StateStreetBank
+{
+    private static Scanner read;
+    private static Scanner input;
+
+    public static void main(String[] args)
+    {
+        String filePath = "customers.csv";
+        input = new Scanner(System.in);//Input for command-line interface.
+        //String searchTerm = "4749464064613eb3feaadc";
+        //88042877140228e4355aed
+
+        //GUI version of input.
+        String searchTerm = JOptionPane.showInputDialog(null, 
+        "Please enter account ID: ", "State Street Bank", 
+        JOptionPane.INFORMATION_MESSAGE);
+        searchTerm = searchTerm.toLowerCase();
+
+        readRecord(searchTerm, filePath);
+    }
+
+    public static void readRecord(String searchTerm, String filePath)
+    {
+        boolean found = false;
+
+        //Nine attributes of a customer record.
+        String account = "";
+        String company = "";
+        String firstName = "";
+        String lastName = "";
+        String address1 = "";
+        String address2 = "";
+        String city = "";
+        String state = "";
+        String zipCode = "";
+
+        try {
+            read = new Scanner(new File(filePath));//Opens customer.csv file
+            read.useDelimiter("[,\n]");//Delimiter to parse String tokens.
+
+            while(read.hasNext() && !found)//while loop to search customer record.
+            {
+                account = read.next();
+                company = read.next();
+                firstName = read.next();
+                lastName = read.next();
+                address1 = read.next();
+                address2 = read.next();
+                city = read.next();
+                state = read.next();
+                zipCode = read.next();
+
+                if(account.equals(searchTerm))
+                {
+                    found = true;
+                }
+            }
+
+            if(found)
+            {
+                String transID = transaction();
+                /*JOptionPane.showMessageDialog(null,"Account: " + account + "\nCompany: " +
+                company + "\nFirst Name: " + firstName + "\nLast Name: " +
+                 lastName + "\nTransaction ID: " + transID);*/
+                JOptionPane.showMessageDialog(null,"Transaction ID: " + transID);
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, "Invalid Account ID.");
+            }  
+        } 
+        catch (Exception e) {
+
+            System.out.println("Error");
+        }
+    }
+
+    public static String transaction()
+    {
+        TransactionID t1 = new TransactionID();//Declare TransactionID class object.
+        String transID = t1.getTransactionID();
+        return transID;
+    }
+}
