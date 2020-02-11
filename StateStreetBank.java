c35806ffb1f7897c3df79b0d73f35568b85d8f7f
AVN64 <60475192+AVN64@users.noreply.github.com>
2020-02-11 14:09:32 -0500

    Add files via upload

--- StateStreetBank.java
+++ StateStreetBank.java
@@ -12,14 +12,14 @@ public class StateStreetBank
     private static Scanner input;//Scanner class object for command-line input.
 
     public static void main(String[] args)
-    {   
-        //GUI version of input.
+    {    
         String filePath = null;
         try {
             input = new Scanner(System.in);//Input for command-line interface.
             //System.out.println("Please enter filename of database: ");
             //filePath = input.nextLine();
 
+            //GUI version of input.
             filePath = JOptionPane.showInputDialog(null, 
             "Please enter filename of database: ", "State Street Bank", 
             JOptionPane.INFORMATION_MESSAGE);
@@ -34,21 +34,40 @@ public class StateStreetBank
             //System.out.println("File not found.");
             System.exit(0);
         }
+        int attempt = 0;
+        String transID;
+        do{	
+            //System.out.println("Please enter account ID: ");
+            //String searchTerm = input.nextLine();
 
-        //System.out.println("Please enter account ID: ");
-        //String searchTerm = input.nextLine();
-        
-        String searchTerm = JOptionPane.showInputDialog(null, 
-        "Please enter account ID: ", "State Street Bank", 
-        JOptionPane.INFORMATION_MESSAGE);
-        searchTerm = searchTerm.toLowerCase();
+			String searchTerm = JOptionPane.showInputDialog(null, 
+            "Please enter account ID: ", "State Street Bank", 
+            JOptionPane.INFORMATION_MESSAGE);
+            searchTerm = searchTerm.toLowerCase();
 
-        //Call method to obtain transaction ID.
-        String transID = getRecord(searchTerm, filePath);
+            //Call method to obtain transaction ID.
+            transID = getRecord(searchTerm, filePath);
+            
+            if(transID.equals("Invalid."))//When incorrect account ID is typed.
+	        {
+	       	    JOptionPane.showMessageDialog(null, "Invalid account ID.", "Error", 
+                JOptionPane.ERROR_MESSAGE);
+                //System.out.println("Invalid account ID.");
+                attempt++;
+            }
+            if(attempt >= 3)//After three attempts, program shuts down.
+            {
+                JOptionPane.showMessageDialog(null, "You made three attempts. Program exiting.",
+                "Error", JOptionPane.ERROR_MESSAGE);
+                //System.out.println("You made three attempts. Program exiting.");
+                System.exit(0);
+            }
+		}while(transID.equals("Invalid."));
+                
         JOptionPane.showMessageDialog(null,"Transaction ID: " + transID);
-
         //System.out.println(transID);
-    }
+
+    }//End of main method.
 
     //Method to read and search records in a csv file using Scanner class object.
     public static String getRecord(String searchTerm, String filePath)
@@ -56,87 +75,24 @@ public class StateStreetBank
         //Initialize TransactionID class object.
         TransactionID t1 = new TransactionID(filePath);
 
+        //Calls method from TransactionID class to obtain String array of transaction ID's.
         String[][] customer = t1.readRecord();
         String transID = "";
         String account = "";
 
         for(int index = 1; index < customer.length; index++)
         {
-            account = customer[index][0];
-            if(account.equals(searchTerm))
-            {
-                //Calls method from TransactionID class to obtain transaction ID.
-                transID = customer[index][9];
+            account = customer[index][0];//Reads account ID's from array.
+            if(account.equals(searchTerm))//Checks for matching account ID from query.
+            {             
+                transID = customer[index][9];//Obtains transaction ID and ends loop.
                 break;
             }
             else
             {
-                transID = "Invalid Account ID.";
+                transID = "Invalid.";
             }
         }
         return transID;
     }//End of getRecord method.
-
-    /*************************** Alternative Method ******************************/
-    //Method to read and search records in a csv file using Scanner class object.
-    /*public static String getRecord(String searchTerm, String filePath)
-    {
-        //Initialize TransactionID class object.
-        TransactionID t1 = new TransactionID(filePath);
-
-        boolean found = false;//Boolean value to determine correct account ID.
-        String transID = null;
-
-        //Nine attributes of a customer record.
-        String account = "";
-        String company = "";
-        String firstName = "";
-        String lastName = "";
-        String address1 = "";
-        String address2 = "";
-        String city = "";
-        String state = "";
-        String zipCode = "";
-
-        try {
-            read = new Scanner(new File(filePath));//Opens customer.csv file.
-            read.useDelimiter("[,\n]");//Delimiter to parse String tokens.
-
-            while(read.hasNext() && !found)//while loop to search customer records.
-            {
-                account = read.next();
-                company = read.next();
-                firstName = read.next();
-                lastName = read.next();
-                address1 = read.next();
-                address2 = read.next();
-                city = read.next();
-                state = read.next();
-                zipCode = read.next();
-
-                if(account.equals(searchTerm))
-                {
-                    found = true;
-                }
-            }
-            //Conditional statement where valid account ID recieves transaction ID.
-            if(found)
-            {
-                //Calls method from TransactionID class to obtain transaction ID.
-                transID = t1.getTransactionID();
-            }
-            else
-            {
-                JOptionPane.showMessageDialog(null, "Invalid Account ID.",
-                 "State Street Bank", JOptionPane.ERROR_MESSAGE);
-            }  
-        } 
-        catch (Exception e) {
-            //Displays if csv file is not found or typed correctly.
-            JOptionPane.showMessageDialog(null, "File not found.", "State Street Bank",
-            JOptionPane.ERROR_MESSAGE);
-            //System.out.println("File not found.");
-        }
-        return transID;//Send transaction ID to main method.
-    }//End of getRecord method.*/
 }//End of StateStreetBank class.
