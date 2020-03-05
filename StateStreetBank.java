53d521446e9c8f56d61ccd80b2bf8badd79a409e
AVN64 <60475192+AVN64@users.noreply.github.com>
2020-03-05 07:48:35 -0500

    GUI version of TransactionTest.

--- StateStreetBank.java
+++ StateStreetBank.java
@@ -1,98 +1,53 @@
-/*Programmed by Group 2 of CSCI 362
-Bank application that generates a random transaction ID for an account ID entered by
-the user. Calls TransactionID class object to recieve random transaction ID.*/
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+/*GUI version of State Street Bank transaction ID generator test class programmed
+by Group 2 of CSCI 362. This program displays transaction ID's generated from 
+TransactionID class that reads the customers.csv file. Imports six classes used
+to display list in a window with scrollbar.*/
+import java.awt.Dimension;
+import javax.swing.DefaultListModel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JViewport;
 
 public class StateStreetBank
 {
-    private static Scanner read;//Scanner class object to read and parse csv file.
-    private static Scanner input;//Scanner class object for command-line input.
+   public static void main(String[] args)
+   {
+        //Initialize TransactionID class object t1.
+        TransactionID t1 = new TransactionID();
 
-    public static void main(String[] args)
-    {    
-        String filePath = null;
-        try {
-            input = new Scanner(System.in);//Input for command-line interface.
-            //System.out.println("Please enter filename of database: ");
-            //filePath = input.nextLine();
+        /*Call readRecord method from TransactionID class to obtain randomly
+        generated transaction ID's for all customers in the csv file.*/
+        final String[][] customer = t1.readRecord();
 
-            //GUI version of input.
-            filePath = JOptionPane.showInputDialog(null, 
-            "Please enter filename of database: ", "State Street Bank", 
-            JOptionPane.INFORMATION_MESSAGE);
+        DefaultListModel<String> lModel = new DefaultListModel<>();
+        JList<String> transactionList = new JList<>(lModel);
+        transactionList.setVisibleRowCount(20);// Views 20 transaction ID's in a box.
 
-            read = new Scanner(new File(filePath));
-        } 
-        catch (Exception e)
-        {
-            //Displays if csv file is not found or typed correctly.
-            JOptionPane.showMessageDialog(null, "File not found.", "State Street Bank",
-            JOptionPane.ERROR_MESSAGE);
-            //System.out.println("File not found.");
-            System.exit(0);
-        }
-        int attempt = 0;
-        String transID;
-        do{	
-            //System.out.println("Please enter account ID: ");
-            //String searchTerm = input.nextLine();
-
-			String searchTerm = JOptionPane.showInputDialog(null, 
-            "Please enter account ID: ", "State Street Bank", 
-            JOptionPane.INFORMATION_MESSAGE);
-            searchTerm = searchTerm.toLowerCase();
-
-            //Call method to obtain transaction ID.
-            transID = getRecord(searchTerm, filePath);
-            
-            if(transID.equals("Invalid."))//When incorrect account ID is typed.
-	        {
-	       	    JOptionPane.showMessageDialog(null, "Invalid account ID.", "Error", 
-                JOptionPane.ERROR_MESSAGE);
-                //System.out.println("Invalid account ID.");
-                attempt++;
-            }
-            if(attempt >= 3)//After three attempts, program shuts down.
-            {
-                JOptionPane.showMessageDialog(null, "You made three attempts. Program exiting.",
-                "Error", JOptionPane.ERROR_MESSAGE);
-                //System.out.println("You made three attempts. Program exiting.");
-                System.exit(0);
-            }
-		}while(transID.equals("Invalid."));
-                
-        JOptionPane.showMessageDialog(null,"Transaction ID: " + transID);
-        //System.out.println(transID);
-
-    }//End of main method.
-
-    //Method to read and search records in a csv file using Scanner class object.
-    public static String getRecord(String searchTerm, String filePath)
-    {
-        //Initialize TransactionID class object.
-        TransactionID t1 = new TransactionID(filePath);
-
-        //Calls method from TransactionID class to obtain String array of transaction ID's.
-        String[][] customer = t1.readRecord();
-        String transID = "";
-        String account = "";
-
+        /*For loop to append transaction ID's to the toString method of
+        Stringbuilder class object sb based on the row length of customer 2D array.*/
         for(int index = 1; index < customer.length; index++)
         {
-            account = customer[index][0];//Reads account ID's from array.
-            if(account.equals(searchTerm))//Checks for matching account ID from query.
-            {             
-                transID = customer[index][9];//Obtains transaction ID and ends loop.
-                break;
-            }
-            else
-            {
-                transID = "Invalid.";
-            }
+            StringBuilder sb = new StringBuilder();
+            sb.append(String.format("%-3d %s%n", index, customer[index][9]));
+               
+            lModel.addElement(sb.toString());
         }
-        return transID;
-    }//End of getRecord method.
+
+        //Initialize JScrollPane class object with transactionList used as an argument.
+        JScrollPane scrollPane = new JScrollPane(transactionList);
+
+        //Adding vertical scrollbar to transactionList.
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        //Setting dimensions of the viewable box of transactionList.
+        JViewport viewport = scrollPane.getViewport();
+        int w = 250;
+        int h = viewport.getPreferredSize().height;
+        Dimension preferredSize = new Dimension(w, h);
+        viewport.setPreferredSize(preferredSize);
+
+        //Window to display transactionList when StateStreetBank program is executed.
+        JOptionPane.showMessageDialog(null, scrollPane, "State Street Bank", JOptionPane.INFORMATION_MESSAGE);
+   }//End of main method.
 }//End of StateStreetBank class.
