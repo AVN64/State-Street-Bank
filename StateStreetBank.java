4a171bd3f0174933d11fafa3b50f6a9dcd0efb07
AVN64 <60475192+AVN64@users.noreply.github.com>
2021-03-13 11:32:40 -0500

    GUI version of TransactionTest class.

--- StateStreetBank.java
+++ StateStreetBank.java
@@ -1,7 +1,20 @@
-/*GUI version of State Street Bank transaction ID generator test class programmed
-by Group 2 of CSCI 362. This program displays transaction ID's generated from 
-TransactionID class that reads the customers.csv file. Imports six classes used
-to display list in a window with scrollbar.*/
+/*
+ * StateStreetBank.java
+ *
+ * Date: 03/20/20
+ *
+ * @author Andy Nguyen
+ *
+ * Description: GUI version of State Street Bank transaction ID generator test class programmed
+ * by Group 2 of CSCI 362. This program displays transaction ID's generated from the
+ * TransactionID class that reads the customers.csv file. Imports six classes used
+ * to implement user input dialog windows and display list in a window with scrollbar.
+ *
+ * Notes:
+ * 03/20/20 - Initial development.
+ * 03/13/21 - Added user input for oneTransID method from Customer class.
+ *
+ */
 import java.awt.Dimension;
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
@@ -16,38 +29,82 @@ public class StateStreetBank
         //Initialize Customer class object c1.
         Customer c1 = new Customer();
 
-        /*Call allTransIDs method from Customer class to obtain randomly
-        generated transaction ID's for all customers in the csv file.*/
-        final String[][] customer = c1.allTransIDs();
+        //Window title for GUI application.
+        final String title = "State Street Bank";
+        //Buttons for user input.
+        final Object[] options = {"Generate All", "Enter Account ID", "Cancel"};
+        
+        //Display options for obtaining transaction IDs from customer database.
+        int choice = JOptionPane.showOptionDialog(null,
+        "Select option to obtain transaction IDs.", title,
+        JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE,
+        null, options, options[2]);
 
-        DefaultListModel<String> lModel = new DefaultListModel<>();
-        JList<String> transactionList = new JList<>(lModel);
-        transactionList.setVisibleRowCount(20);// Views 20 transaction ID's in a box.
-
-        /*For loop to append transaction ID's to the toString method of
-        Stringbuilder class object sb based on the row length of customer 2D array.*/
-        for(int index = 1; index < customer.length; index++)
+        //When user selects "Generate All" option.
+        if(choice == JOptionPane.YES_OPTION)
         {
-            StringBuilder sb = new StringBuilder();
-            sb.append(String.format("%-3d %s%n", index, customer[index][9]));
+            /*Call allTransIDs method from Customer class to obtain randomly
+            generated transaction ID's for all customers in the csv file.*/
+            final String[][] customer = c1.allTransIDs();
+
+            DefaultListModel<String> lModel = new DefaultListModel<>();
+            JList<String> transactionList = new JList<>(lModel);
+            transactionList.setVisibleRowCount(20);// Views 20 transaction ID's in a box.
+
+            /*For loop to append transaction ID's to the toString method of
+            Stringbuilder class object sb based on the row length of customer 2D array.*/
+            for(int index = 1; index < customer.length; index++)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.append(String.format("%-3d %s%n", index, customer[index][9]));
                
-            lModel.addElement(sb.toString());
+                lModel.addElement(sb.toString());
+            }
+
+            //Initialize JScrollPane class object with transactionList used as an argument.
+            JScrollPane scrollPane = new JScrollPane(transactionList);
+
+            //Adding vertical scrollbar to transactionList.
+            scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+            //Setting dimensions of the viewable box of transactionList.
+            JViewport viewport = scrollPane.getViewport();
+            int w = 250;
+            int h = viewport.getPreferredSize().height;
+            Dimension preferredSize = new Dimension(w, h);
+            viewport.setPreferredSize(preferredSize);
+
+            //Window to display transactionList when StateStreetBank program is executed.
+            JOptionPane.showMessageDialog(null, scrollPane, title, JOptionPane.INFORMATION_MESSAGE);
+            System.exit(0);
+        }
+        //When user selects the "Enter Account ID option."
+        else if(choice == JOptionPane.NO_OPTION)
+        {
+            //Prompt user to enter a valid account ID.
+            String account = JOptionPane.showInputDialog(null, "Enter Account ID.", title, 
+            JOptionPane.QUESTION_MESSAGE);
+            String transID = c1.oneTransID(account);
+
+            /*Conditonal if statement based on what the oneTransID method from
+            Customer class object returns.*/
+            if(transID == "Invalid.")
+            {
+                JOptionPane.showMessageDialog(null, "Invalid Account ID. Program exiting.",
+                 title, JOptionPane.ERROR_MESSAGE);
+                System.exit(0);
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null, transID, title, JOptionPane.INFORMATION_MESSAGE);
+                System.exit(0);
+            }              
+        }
+        //When user selects the "Cancel" option.
+        else
+        {
+            System.exit(0);
         }
 
-        //Initialize JScrollPane class object with transactionList used as an argument.
-        JScrollPane scrollPane = new JScrollPane(transactionList);
-
-        //Adding vertical scrollbar to transactionList.
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-
-        //Setting dimensions of the viewable box of transactionList.
-        JViewport viewport = scrollPane.getViewport();
-        int w = 250;
-        int h = viewport.getPreferredSize().height;
-        Dimension preferredSize = new Dimension(w, h);
-        viewport.setPreferredSize(preferredSize);
-
-        //Window to display transactionList when StateStreetBank program is executed.
-        JOptionPane.showMessageDialog(null, scrollPane, "State Street Bank", JOptionPane.INFORMATION_MESSAGE);
    }//End of main method.
 }//End of StateStreetBank class.
